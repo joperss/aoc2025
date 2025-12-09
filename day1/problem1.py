d52d6bd6855a06de0a719221fc430ceb72a13d7c
Johan Persson <persson.johan.92@gmail.com>
2025-12-09 23:18:40 +0100

    day 1 problem 2

--- day1/problem1.py
+++ day1/problem1.py
@@ -2,16 +2,16 @@
 
 
 class Node:
-    def __init__(self, data, prev, next) -> None:
+    def __init__(self, data: int, prev, next) -> None:
         self.data = data
         self.next = next
         self.prev = prev
 
 
 class Dial:
-    def __init__(self, start, stop, start_pos) -> None:
-        self.head: Node = Node(start, None, None)
-        self.curr: Node = self.head
+    def __init__(self, start: int, stop: int, start_pos: int) -> None:
+        self.head = Node(start, None, None)
+        self.curr = self.head
         for i in range(start + 1, stop):
             node = Node(i, self.curr, None)
             self.curr.next = node
@@ -24,26 +24,50 @@ class Dial:
 
         self.step(start_pos, "R")
 
-    def step(self, steps, dir):
+    def step(self, steps, dir) -> tuple[int, int]:
+        zero_count = 0
         if dir == "L":
             while steps > 0:
                 self.curr = self.curr.prev
+                if self.curr.data == 0:
+                    zero_count += 1
                 steps -= 1
         elif dir == "R":
             while steps > 0:
                 self.curr = self.curr.next
+                if self.curr.data == 0:
+                    zero_count += 1
                 steps -= 1
+        return self.curr.data, zero_count
 
 
-dial = Dial(0, 99, 50)
-code = 0
-with open("input.txt", "r") as file:
-    for line in file:
-        line = line.strip()
-        dir = line[0]
-        steps = int(line[1:])
-        dial.step(steps, dir)
-        if dial.curr.data == 0:
-            code += 1
+def problem1() -> int:
+    dial = Dial(0, 99, 50)
+    code = 0
+    with open("input.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            dir = line[0]
+            steps = int(line[1:])
+            pos, _ = dial.step(steps, dir)
+            if pos == 0:
+                code += 1
 
-print(code)
+    return code
+
+
+def problem2() -> int:
+    dial = Dial(0, 99, 50)
+    code = 0
+    with open("input.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            dir = line[0]
+            steps = int(line[1:])
+            _, zero_count = dial.step(steps, dir)
+            code += zero_count
+
+    return code
+
+
+print(problem2())
