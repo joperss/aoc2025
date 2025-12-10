0a82dc4ff29bf05cec8c33224777eddb98c6624f
Johan Persson <persson.johan.92@gmail.com>
2025-12-10 20:55:03 +0100

    Day 2 problem 1; greedy

--- day2/day2.py
+++ day2/day2.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 with open("input.txt", "r") as file:
+    invalid_id_count = 0
     for x in file.readline().split(","):
         start, stop = x.split("-")
-        print(start, stop)
+
+        for i in range(int(start), int(stop) + 1):
+            i_str = str(i)
+            i_len = len(i_str)
+            if i_len % 2 != 0:
+                pass
+
+            pre = i_str[: int(i_len / 2)]
+            post = i_str[int(i_len / 2) :]
+            if pre == post:
+                invalid_id_count += i
+
+    print("Invalid ID count: " + str(invalid_id_count))
