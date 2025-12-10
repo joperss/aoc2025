208a56ce0575dc8840b851b40b5ff0aa0d89cfb2
Johan Persson <persson.johan.92@gmail.com>
2025-12-10 21:36:28 +0100

    Day 2, problem 1: optimize search start

--- day2/day2.py
+++ day2/day2.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
+from math import floor
+
 with open("input.txt", "r") as file:
     invalid_id_count = 0
     for x in file.readline().split(","):
-        start, stop = x.split("-")
-        start = int(start)
-        stop = int(stop)
+        start_str, stop_str = x.split("-")
+        start = int(start_str)
+        stop = int(stop_str)
 
-        for i in range(1, stop):
+        search_start_str = (
+            start_str[: int(len(start_str) / 2)]
+            if len(start_str) % 2 == 0
+            else "1" + "0" * floor(len(start_str) / 2)
+        )
+        for i in range(int(search_start_str), stop):
             i_str = str(i)
             invalid_id = int(i_str + i_str)
             if invalid_id > stop:
